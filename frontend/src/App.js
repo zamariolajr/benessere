b11553863066020b1d9d352e064d8f0f81186495
Jose Zamariola <zamariolajr@gmail.com>
2022-07-17 21:33:01 -0300

    First commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Benessere</a>
+      </header>
+      <main>list products</main>
+    </div>
+  )
+}
+
+export default App
